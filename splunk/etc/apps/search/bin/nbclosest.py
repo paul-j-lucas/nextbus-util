729e30bb2852c73789bb2a43bda3f9e174cd0f21
Paul J. Lucas <plucas@splunk.com>
2016-08-29 07:22:28 -0700

    Now using v2 (chunked) search protocol.

--- splunk/etc/apps/search/bin/nbclosest.py
+++ splunk/etc/apps/search/bin/nbclosest.py
@@ -6,9 +6,11 @@
 #   Author:         Paul J. Lucas
 ##
 
-import csv
 import operator
 import sys
+
+from splunklib.searchcommands import dispatch, Configuration, EventingCommand
+from splunklib.searchcommands.decorators import ConfigurationSetting
 
 ###############################################################################
 
@@ -19,39 +21,57 @@ K_VID   = 'vehicle_id'
 
 ###############################################################################
 
-vehicle_dict = { }
+@Configuration()
+class NextBusClosestStop( EventingCommand ):
+    """ Filters NextBus records to only those where a vehicle is at its
+        closest distance to a given stop.
 
-try:
-    reader = csv.DictReader( sys.stdin )
-    headers = reader.fieldnames
-    writer = csv.DictWriter( sys.stdout, headers )
-    writer.writeheader()
+    ##Syntax
 
-    for row in reader:
-        vid = row[ K_VID ]
-        if vid not in vehicle_dict:
-            vehicle_dict[ vid ] = row
-        else:
-            old_row = vehicle_dict[ vid ]
-            old_stop = old_row[ K_STAG ]
-            new_stop =     row[ K_STAG ]
-            if new_stop == old_stop:
-                old_dist = int( old_row[ K_VDIST ] )
-                new_dist = int(     row[ K_VDIST ] )
-                if new_dist <= old_dist:
-                    vehicle_dict[ vid ] = row
+    .. code-block::
+        nbclosest
+
+    ##Description
+
+    The :code:`nbclosest` command filters NextBus records to only those where a
+    vehicle is at its closest distance to a given stop.
+    """
+
+    class ConfigurationSettings( EventingCommand.ConfigurationSettings ):
+        required_fields = ConfigurationSetting(
+            value=[ K_TIME, K_VID, K_VDIST, K_STAG ]
+        )
+
+
+    def __init__( self ):
+        super( NextBusClosestStop, self ).__init__()
+        self.vdict = { }
+
+
+    def transform( self, records ):
+        for rec in records:
+            vid = rec[ K_VID ]
+            if vid not in self.vdict:
+                self.vdict[ vid ] = rec
             else:
-                writer.writerow( old_row )
-                vehicle_dict[ vid ] = row
+                old_rec = self.vdict[ vid ]
+                old_stop = old_rec[ K_STAG ]
+                new_stop =     rec[ K_STAG ]
+                if new_stop == old_stop:
+                    old_dist = int( old_rec[ K_VDIST ] )
+                    new_dist = int(     rec[ K_VDIST ] )
+                    if new_dist <= old_dist:
+                        self.vdict[ vid ] = rec
+                else:
+                    yield old_rec
+                    self.vdict[ vid ] = rec
 
-    remaining_rows = vehicle_dict.values()
-    for row in sorted( remaining_rows, key=operator.itemgetter( K_TIME ) ):
-        writer.writerow( row )
+        remaining_recs = self.vdict.values()
+        for rec in sorted( remaining_recs, key=operator.itemgetter( K_TIME ) ):
+            yield rec
 
-except Exception as e:
-    import traceback
-    stack = traceback.format_exc()
-    print >>sys.stderr, "Unhandled exception: %s; %s" % (e, stack)
+
+dispatch( NextBusClosestStop, sys.argv, sys.stdin, sys.stdout, __name__ )
 
 ###############################################################################
 # vim:set et sw=4 ts=4:
